c3627db3b8c762adc671f8e24eae980527de37ab
jakub.jozwiak <kubencki@gmail.com>
2021-01-10 19:25:43 +0100

    feat: base authentication

--- packages/api/src/app.controller.ts
+++ packages/api/src/app.controller.ts
@@ -1,12 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthService } from './auth/auth.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private authService: AuthService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req: any) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req: any) {
+    return req.user;
+  }
+
+  @Get('public-route')
+  testPublicRoute() {
+    return 'It works';
+  }
+
+  @Get('not-public-route')
+  testNotPublicRoute() {
+    return 'It works';
   }
 }
