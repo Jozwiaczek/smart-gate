b96ea72d54fcea30ee9916afc04b5ddb70a8f3c3
jakub.jozwiak <kubencki@gmail.com>
2021-01-22 21:27:10 +0100

    fix(smart-gate-api): remove build dir, add .env.example, add db docs

--- packages/api/build/src/user/user.type.d.ts
+++
@@ -1,9 +0,0 @@
-import { Role } from '../auth/role.enum';
-export declare class UserEntity {
-    id: string;
-    email: string;
-    password: string;
-    firstName: string | null;
-    lastName: string | null;
-    roles: Array<Role>;
-}
