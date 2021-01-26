755db93b733e9cff7537f83406309fdcf9bc74b7
Mateusz Nestorowicz <nestorowicz1@gmail.com>
2021-01-26 01:05:13 +0100

    fix: remove build folder

--- packages/api/build/src/auth/guards/only-admin.guard.d.ts
+++
@@ -1,7 +0,0 @@
-import { CanActivate } from '@nestjs/common';
-import { AuthService } from '../auth.service';
-export declare class OnlyAdminGuard implements CanActivate {
-    private readonly authService;
-    constructor(authService: AuthService);
-    canActivate(): Promise<boolean>;
-}
--- packages/api/build/src/auth/guards/only-authenticated.guard.d.ts
+++
@@ -1,7 +0,0 @@
-import { CanActivate } from '@nestjs/common';
-import { AuthService } from '../auth.service';
-export declare class OnlyAuthenticatedGuard implements CanActivate {
-    private readonly authService;
-    constructor(authService: AuthService);
-    canActivate(): Promise<boolean>;
-}
