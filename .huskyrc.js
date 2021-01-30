0d6a161dbb2d0f97ca6f885451cb1a2ef205d1b5
jakub.jozwiak <kubencki@gmail.com>
2021-01-30 02:41:01 +0100

    fix(client): adjust element generator and regarding that elements

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   hooks: {
-    'pre-commit': 'yarn type-check && lint-staged',
-    'pre-push': 'yarn test && yarn test:e2e',
+    'pre-commit': 'yarn type-check && lint-staged && echo precommit',
+    'pre-push': 'yarn test && yarn test:e2e && echo prepush',
     'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
   },
 };
