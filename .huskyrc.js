832ebd2a547e60d1eaf5f704f1feae56cff7e0e8
jakub.jozwiak <kubencki@gmail.com>
2021-01-30 02:49:14 +0100

    .

--- .huskyrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  hooks: {
-    'pre-commit': 'yarn type-check && lint-staged && echo pre',
-    'pre-push': 'yarn test && yarn test:e2e && echo prepush',
-    'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
-  },
-};
