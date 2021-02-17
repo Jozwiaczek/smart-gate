190095d6b6d54246289bede88d391a6514de7018
Mateusz Nestorowicz <nestorowicz1@gmail.com>
2021-02-17 21:37:55 +0100

    fix: create random user method

--- packages/api/test/utils/testCreateRandomUser.ts
+++ packages/api/test/utils/testCreateRandomUser.ts
@@ -4,7 +4,7 @@ import { UserEntity } from '../../src/database/entities/user.entity';
 
 export const testCreateRandomUser = async (connection: Connection): Promise<UserEntity> => {
   const userEntity = new UserEntity();
-  userEntity.email = `${Date.now()}@test-email.com`;
+  userEntity.email = `${Date.now()}${Math.random()}@test-email.com`;
   userEntity.password = `${Date.now()}password`;
   await connection.getRepository(UserEntity).save(userEntity);
   return userEntity;
