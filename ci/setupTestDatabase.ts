827c2bcfd673e8cb3826a458ae06b57613b26c31
jakub.jozwiak <kubencki@gmail.com>
2021-02-18 00:06:15 +0100

    ci: add ci timeout, setup db for ci, add ci tests step

---
+++ ci/setupTestDatabase.ts
@@ -0,0 +1,66 @@
+import pg from 'pg';
+
+interface CreateClientAndConnectParams {
+  host: string;
+  port: number;
+  user: string;
+  password: string;
+  database?: string;
+}
+
+const createClientAndConnect = async ({
+  host,
+  port,
+  user,
+  password,
+  database,
+}: CreateClientAndConnectParams) => {
+  const client = new pg.Client({
+    host,
+    port,
+    user,
+    password,
+    database,
+  });
+
+  await client.connect();
+
+  return client;
+};
+
+const setupTestDatabase = async (): Promise<void> => {
+  const missingVariables = ['DB_USERNAME', 'DB_ADMIN_PASSWORD'].filter((key) => !process.env[key]);
+  if (missingVariables.length) {
+    throw new Error(`Missing environment variables: ${missingVariables.join(', ')}`);
+  }
+
+  const adminClient = await createClientAndConnect({
+    host: process.env.DB_HOST || 'localhost',
+    port: +(process.env.DB_PORT as string) || 5432,
+    user: process.env.DB_ADMIN_USER as string,
+    password: process.env.DB_ADMIN_PASSWORD as string,
+  });
+
+  await adminClient.query('CREATE DATABASE smart-gate-db-test');
+  await adminClient.query("CREATE USER sg WITH ENCRYPTED PASSWORD 'sg'");
+  await adminClient.query('GRANT ALL PRIVILEGES ON DATABASE smart-gate-db-test TO sg');
+
+  const testDatabaseClient = await createClientAndConnect({
+    host: process.env.DB_HOST || 'localhost',
+    port: +(process.env.DB_PORT as string) || 5432,
+    user: process.env.DB_ADMIN_USER as string,
+    password: process.env.DB_ADMIN_PASSWORD as string,
+    database: 'smart-gate-db-test',
+  });
+
+  await testDatabaseClient.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+};
+
+setupTestDatabase()
+  .then(() => {
+    process.exit();
+  })
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
