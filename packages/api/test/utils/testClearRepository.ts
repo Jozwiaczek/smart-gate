721d9fb5c6d2a9476e6ef4a35d0a0bd5afc734c9
Mefjus <nestorowicz1@gmail.com>
2021-02-18 03:37:46 +0100

    User service unit test (#54)
    * fix: create random user method
    
    * test: add userService unit test
    
    * fix: findAll() unit test
    
    * fix: clarify users findAll test
    
    * fix: clarify users findAll test
    
    * test: add testClearRepository.ts
    
    * test: wip
    
    * test: simplify userService test
    
    Co-authored-by: jakub.jozwiak <kubencki@gmail.com>

---
+++ packages/api/test/utils/testClearRepository.ts
@@ -0,0 +1,13 @@
+import { Connection, EntityTarget, Repository } from 'typeorm';
+
+export const testClearRepository = async <T>(
+  connection: Connection,
+  entity: EntityTarget<T>,
+): Promise<Repository<T>> => {
+  const repository = connection.getRepository(entity);
+  await repository.delete({});
+
+  await expect(repository.count()).resolves.toEqual(0);
+
+  return repository;
+};
