ea9cf600126beaf1495276f75b4b2062b2d479ed
Jakub Jóźwiak <kubencki@gmail.com>
2021-02-18 22:41:51 +0100

    Remove test check from pre-push git hooks and add linters in ci (#61)
    * ci: remove test check from pre-push git hooks
    
    * style: fix code formatting
    
    * fix: change dependabot package-ecosystem to npm
    
    * ci: check containers default versioning
    
    * ci:  add linters in GH pipeline

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,7 +1,6 @@
 module.exports = {
   hooks: {
     'pre-commit': 'yarn type-check && lint-staged',
-    'pre-push': 'yarn test && yarn test:e2e',
     'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
   },
 };
