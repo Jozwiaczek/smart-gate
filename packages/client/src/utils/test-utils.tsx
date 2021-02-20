06f9f4b12892782020dcd351e6f3394e585eafd6
jakub.jozwiak <kubencki@gmail.com>
2021-02-20 03:24:18 +0100

    fix(client): add workaround for test global variables

--- packages/client/src/utils/test-utils.tsx
+++ packages/client/src/utils/test-utils.tsx
@@ -8,6 +8,20 @@ const AllTheProviders: FC = ({ children }) => <StylesProvider>{children}</Styles
 const customRender = (ui: ReactElement, options?: Omit<RenderOptions, 'queries'>) =>
   render(ui, { wrapper: AllTheProviders, ...options });
 
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // Deprecated
+    removeListener: jest.fn(), // Deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
+
 export * from '@testing-library/react';
 
 export { customRender as render };
