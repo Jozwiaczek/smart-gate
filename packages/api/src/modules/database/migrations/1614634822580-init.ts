426313ca3f4e67b7f7795abe065c984dc9dc0e33
Mateusz Nestorowicz <nestorowicz1@gmail.com>
2021-03-01 22:41:43 +0100

    fix(api): generate new migration

--- packages/api/src/modules/database/migrations/1614634822580-init.ts
+++ packages/api/src/modules/database/migrations/1614634822580-init.ts
@@ -1,9 +1,10 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class Init1614620651933 implements MigrationInterface {
-  name = 'Init1614620651933';
+export class init1614634822580 implements MigrationInterface {
+  name = 'init1614634822580';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query("CREATE TYPE \"users_roles_enum\" AS ENUM('user', 'admin')");
     await queryRunner.query(
       'CREATE TABLE "users" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "email" character varying NOT NULL, "password" character varying NOT NULL, "firstName" character varying, "lastName" character varying, "roles" "users_roles_enum" array, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))',
     );
@@ -23,5 +24,6 @@ export class Init1614620651933 implements MigrationInterface {
     await queryRunner.query('DROP TABLE "refresh_tokens"');
     await queryRunner.query('DROP INDEX "IDX_97672ac88f789774dd47f7c8be"');
     await queryRunner.query('DROP TABLE "users"');
+    await queryRunner.query('DROP TYPE "users_roles_enum"');
   }
 }
