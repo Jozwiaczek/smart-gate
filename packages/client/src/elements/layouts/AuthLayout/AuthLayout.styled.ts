997509273947105d9a6b269945904d2a98950b36
jakub.jozwiak <kubencki@gmail.com>
2021-03-09 02:46:05 +0100

    fix(client): margin for actions container

--- packages/client/src/elements/layouts/AuthLayout/AuthLayout.styled.ts
+++ packages/client/src/elements/layouts/AuthLayout/AuthLayout.styled.ts
@@ -10,7 +10,7 @@ export const StyledActionsContainer = styled.div<ActionsContainerProps>`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  margin: 20px 0;
+  margin-top: 30px;
   flex-wrap: wrap;
   flex-direction: ${({ direction }) => direction};
 
