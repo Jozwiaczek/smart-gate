e1c43b79e98b42fb365667bbbca64066a2d3c9be
jakub.jozwiak <kubencki@gmail.com>
2021-03-18 00:04:36 +0100

    feat(api): add styles from design

--- packages/api/src/emailTemplates/welcomeTemplate.ts
+++ packages/api/src/emailTemplates/welcomeTemplate.ts
@@ -6,26 +6,32 @@ const welcomeTemplate = ({ firstName = '' }) => {
     <mjml>
       <mj-head>
         <mj-attributes>
-          <mj-text font-family="Roboto, sans-serif" align="center" font-size="16px" color='#FFF'/>
-          <mj-section background-color="#30444E" border-radius='12px'/>
+          <mj-text font-family="Roboto, sans-serif" align="center" font-size="16px" color='#FFF' line-height='19px'/>
           <mj-button background-color="#3ED598" color="#22343C" font-size="16px" border-radius='12px' />
         </mj-attributes>
-        <mj-style inline="inline">
-          .body-section { -webkit-box-shadow: 1px 4px 11px 0px rgba(0, 0, 0, 0.15); -moz-box-shadow: 1px
-          4px 11px 0px rgba(0, 0, 0, 0.15); box-shadow: 1px 4px 11px 0px rgba(0, 0, 0, 0.15); }
-        </mj-style>
       </mj-head>
       <mj-body>
-        <mj-section padding-bottom="20px" padding-top="10px" css-class="body-section">
+        <mj-section border-radius='12px 12px 0 0' background-color="#22343C" padding='0'>
           <mj-column>
-            <mj-image src='${process.env.CLIENT_URL}/email-logo.png' height='250px' width='250px' alt="header image" />
-            <mj-text padding="10px 25px" font-size="22px">
-              Welcome ${firstName}!
+            <mj-image src='${process.env.CLIENT_URL}/email-images/sg-logo.png' height='140px' width='140px' alt="header image" />
+          </mj-column>
+        </mj-section>
+        <mj-section padding-bottom="20px" padding-top="10px" border-radius='0 0 12px 12px' background-color="#30444E">
+          <mj-column>
+            <mj-text padding="50px 25px" font-size="40px" line-height='47px'>
+              You’ve been invited <br/> to <b>Smart Gate</b>!
             </mj-text>
-            <mj-button href="https://mjml.io" padding="40px 0 0 0" width='150px' height='44px' >
-              Log in
+            <mj-image src='${process.env.CLIENT_URL}/email-images/paper-plane.png' height='160px' width='160px' alt="header image" />
+            <mj-text font-size="24px">
+            Hi <b>${firstName}</b>,
+            </mj-text>
+            <mj-text>
+            It’s look like that administrator invites you to group<br/> in Smart Gate system. Click on the button to<br/> create your account.
+            </mj-text>
+            <mj-button href="https://mjml.io" padding="40px 0 0 0" width='240px' height='45px'>
+              Create Your Account
             </mj-button>
-            <mj-text font-size="14px" padding="40px 0 20px">Best, <br /> The Smart Gate Team
+            <mj-text font-size="14px" color='#96A7AF' padding="70px 0 0">Best, <br /> The Smart Gate Team
             </mj-text>
           </mj-column>
         </mj-section>
