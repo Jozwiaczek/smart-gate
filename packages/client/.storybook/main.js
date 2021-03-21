2797a3ef5bf1111e38212dae0218fdcefd8f10fd
jakub.jozwiak <kubencki@gmail.com>
2021-03-21 18:55:10 +0100

    feat(client): add storybook source and a11y addons

--- packages/client/.storybook/main.js
+++ packages/client/.storybook/main.js
@@ -3,5 +3,10 @@ module.exports = {
     fastRefresh: true,
   },
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-storysource',
+    '@storybook/addon-a11y',
+  ],
 };
