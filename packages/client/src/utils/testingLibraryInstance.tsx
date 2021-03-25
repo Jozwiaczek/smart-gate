c34db0e354ada3fa8885a02479d9577dc6831d52
jakub.jozwiak <kubencki@gmail.com>
2021-03-25 20:46:00 +0100

    fix: add only styling providers for unit tests

--- packages/client/src/utils/testingLibraryInstance.tsx
+++ packages/client/src/utils/testingLibraryInstance.tsx
@@ -1,9 +1,18 @@
 import { render, RenderOptions } from '@testing-library/react';
 import React, { FC, ReactElement } from 'react';
+import { I18nextProvider } from 'react-i18next';
 
-import Providers from '../providers';
+import i18n from '../i18n';
+import SnackbarProvider from '../providers/SnackbarProvider';
+import StylesProvider from '../providers/StylesProvider';
 
-const AllTheProviders: FC = ({ children }) => <Providers>{children}</Providers>;
+const AllTheProviders: FC = ({ children }) => (
+  <StylesProvider>
+    <I18nextProvider i18n={i18n}>
+      <SnackbarProvider>{children}</SnackbarProvider>
+    </I18nextProvider>
+  </StylesProvider>
+);
 
 const customRender = (ui: ReactElement, options?: Omit<RenderOptions, 'queries'>) =>
   render(ui, { wrapper: AllTheProviders, ...options });
