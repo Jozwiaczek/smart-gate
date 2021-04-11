1105b2e559c467ac31fb5c2c133b604ef5a4ae07
jakub.jozwiak <kubencki@gmail.com>
2021-04-11 03:36:51 +0200

    feat: adjust update dev job

--- release.config.js
+++ release.config.js
@@ -5,7 +5,13 @@ module.exports = {
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
     '@semantic-release/github',
-    '@semantic-release/git',
+    [
+      '@semantic-release/git',
+      {
+        // eslint-disable-next-line no-template-curly-in-string
+        message: 'chore: ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
   ],
   parserOpts: {
     mergePatterns: "Merge branch 'dev' into main",
