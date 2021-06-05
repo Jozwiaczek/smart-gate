6bac502ebc5f6254c29d508fab67bcdea9fd1b29
jakub.jozwiak <kubencki@gmail.com>
2021-06-05 15:39:07 +0200

    chore: runs prettier and adjust lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   '**/*.{js,jsx,ts,tsx}': ['eslint --fix --cache'],
-  '**/*.{js,jsx,ts,tsx,md,json}': ['prettier --write --ignore-path .prettierignore'],
+  '**/*.{js,jsx,ts,tsx,md,json,yaml}': ['prettier --write --ignore-path .prettierignore'],
   '**/*.{styled.ts,tsx}': ['stylelint'],
+  '**/package.json': 'sort-package-json',
 };
