83b76c0a91b39bb18a83149eaaf145839a124c75
jakub.jozwiak <kubencki@gmail.com>
2021-06-26 15:29:11 +0200

    chore(device): change pins name

--- packages/device/src/hooks/onOpen.ts
+++ packages/device/src/hooks/onOpen.ts
@@ -1,9 +1,16 @@
-import { OPEN_STATUS_LIGHT_PIN } from '../constants/pins';
+import rpio from 'rpio';
+
+import { CAMERA_POWER, LOCK_POWER, OPEN_STATUS_LIGHT_PIN } from '../constants/pins';
 
 const onOpen = () => {
+  console.log('Opening');
   rpio.write(OPEN_STATUS_LIGHT_PIN, rpio.HIGH);
-  rpio.sleep(5);
+  rpio.write(CAMERA_POWER, rpio.LOW);
+  rpio.write(LOCK_POWER, rpio.LOW);
+  rpio.sleep(10);
   rpio.write(OPEN_STATUS_LIGHT_PIN, rpio.LOW);
+  rpio.write(CAMERA_POWER, rpio.HIGH);
+  rpio.write(LOCK_POWER, rpio.HIGH);
 };
 
 export default onOpen;
