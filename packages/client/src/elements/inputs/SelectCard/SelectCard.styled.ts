bdd0f7800454bd8e171dc59e1d4fc3fb5c4d92e1
jakub.jozwiak <kubencki@gmail.com>
2021-07-23 21:57:27 +0200

    feat(client): add select card item outline styling

--- packages/client/src/elements/inputs/SelectCard/SelectCard.styled.ts
+++ packages/client/src/elements/inputs/SelectCard/SelectCard.styled.ts
@@ -12,22 +12,40 @@ export const CardItemButton = styled.button(
     width: 100%;
     background: none;
     border: none;
+    outline: none;
 
     :hover {
       cursor: pointer;
       background: ${palette.background.paperHover};
     }
+
+    :focus-visible {
+      box-shadow: inset 0 0 0 2px ${palette.primary.light};
+    }
   `,
 );
 
 export const StyledCard = styled(Card)(
-  ({ theme: { palette } }) => css`
+  ({ theme: { palette, sizes } }) => css`
     width: 100%;
     padding: 0;
     overflow: hidden;
 
+    ${CardItemButton}:only-child {
+      border-radius: ${sizes.borderRadius};
+    }
+
+    ${CardItemButton}:not(:first-child):not(:last-child) {
+      border-radius: 0;
+    }
+
+    ${CardItemButton}:not(:first-child) {
+      border-radius: 0 0 ${sizes.borderRadius} ${sizes.borderRadius};
+    }
+
     ${CardItemButton}:not(:last-child) {
       border-bottom: 1px solid ${palette.background.default};
+      border-radius: ${sizes.borderRadius} ${sizes.borderRadius} 0 0;
     }
   `,
 );
--- packages/client/src/pages/authorized/Settings/cards/ThemeCard/index.tsx
+++ packages/client/src/pages/authorized/Settings/cards/ThemeCard/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { ReactNode } from 'react';
+import styled, { css } from 'styled-components';
 
 import { SelectCard } from '../../../../../elements';
 import { ThemeTypeIndicator } from '../../../../../elements/animations';
@@ -14,9 +14,61 @@ const Wrapper = styled.div`
 export const CardHeader = styled.div`
   display: flex;
   align-items: center;
-  height: 42px;
+  height: 46px;
   margin-bottom: 15px;
   gap: 15px;
+`;
+
+export const SunWrapper = styled.div(
+  ({ theme: { palette } }) => css`
+    height: 32px;
+    width: 32px;
+    transform: rotate(80deg);
+    color: ${palette.colors.orange};
+    line {
+      stroke-width: 8;
+      stroke-linecap: round;
+    }
+  `,
+);
+
+const MoonWrapper = styled.div`
+  background: rgb(30, 30, 30);
+  border-radius: 50%;
+  height: 32px;
+  width: 32px;
+`;
+
+const MoonIcon = () => (
+  <MoonWrapper>
+    <svg viewBox="-50 -50 200 200">
+      <circle cx="50" cy="50" r="40" fill="rgb(255, 255, 255)" />
+      <circle cx="72" cy="34" r="40" fill="rgb(30, 30, 30)" />
+    </svg>
+  </MoonWrapper>
+);
+
+const SunIcon = () => (
+  <SunWrapper>
+    <svg viewBox="-50 -50 200 200">
+      <circle cx="50" cy="50" r="40" fill="currentColor" />
+      <line x1="50" y1="-25" x2="50" y2="-5" stroke="currentColor" />
+      <line x1="50" y1="125" x2="50" y2="105" stroke="currentColor" />
+      <line x1="-25" y1="50" x2="-5" y2="50" stroke="currentColor" />
+      <line x1="125" y1="50" x2="105" y2="50" stroke="currentColor" />
+      <line x1="-5" y1="-5" x2="10" y2="10" stroke="currentColor" />
+      <line x1="90" y1="90" x2="105" y2="105" stroke="currentColor" />
+      <line x1="15" y1="85" x2="-5" y2="105" stroke="currentColor" />
+      <line x1="105" y1="-5" x2="85" y2="15" stroke="currentColor" />
+    </svg>
+  </SunWrapper>
+);
+
+const Tmp = styled.div`
+  display: flex;
+  gap: 10px;
+  width: 100%;
+  height: 100%;
 `;
 
 const ThemeCard = () => {
@@ -32,9 +84,19 @@ const ThemeCard = () => {
         <ThemeTypeIndicator themeType={themeType} />
         <h2>Theme</h2>
       </CardHeader>
-      <SelectCard<StoredThemeType> value={storedThemeType} onChange={onChange}>
-        <option value="light">Light</option>
-        <option value="dark">Dark</option>
+      <SelectCard value={storedThemeType} onChange={onChange}>
+        <option value="light">
+          <Tmp>
+            <SunIcon />
+            Light
+          </Tmp>
+        </option>
+        <option value="dark">
+          <Tmp>
+            <MoonIcon />
+            Dark
+          </Tmp>
+        </option>
         <option value="system">System theme</option>
       </SelectCard>
     </Wrapper>
