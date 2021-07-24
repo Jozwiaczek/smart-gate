e1a91ea35fdfc65da4c57e2e8fdf1ab5778487cc
jakub.jozwiak <kubencki@gmail.com>
2021-07-24 03:14:34 +0200

    feat(client): add settings account tabbed section

---
+++ packages/client/src/pages/authorized/Settings/sections/Account/Account.styled.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+import { Card } from '../../../../../elements';
+
+export const StyledCard = styled(Card)`
+  padding: 0;
+  width: 100%;
+`;
+
+export const TabsWrapper = styled.div`
+  height: 80px;
+  width: 100%;
+`;
--- packages/client/src/pages/authorized/Settings/sections/Account/index.tsx
+++ packages/client/src/pages/authorized/Settings/sections/Account/index.tsx
@@ -1,12 +1,50 @@
-import React from 'react';
+import React, { MouseEvent, useState } from 'react';
 
-import { Card } from '../../../../../elements';
+import TabbedLayout from '../../../../../elements/layouts/TabbedLayout';
+import { TabProps } from '../../../../../elements/layouts/TabbedLayout/Tab/Tab.types';
+import { DashboardIcon, HistoryIcon } from '../../../../../icons';
 import SettingsSection from '../SettingsSection';
+import { StyledCard, TabsWrapper } from './Account.styled';
+
+const tabs: Array<TabProps> = [
+  {
+    label: 'Basics',
+    icon: <HistoryIcon />,
+  },
+  {
+    label: 'Actions',
+    icon: <DashboardIcon />,
+  },
+];
 
 const Account = () => {
+  const [activeTab, setActiveTab] = useState(0);
+
+  const handleChange = (event: MouseEvent, newValue: number) => {
+    setActiveTab(newValue);
+  };
+
   return (
     <SettingsSection title="routes.settings.account.title">
-      <Card>Basics</Card>
+      <StyledCard>
+        <TabsWrapper>
+          <TabbedLayout.Tabs
+            value={activeTab}
+            onChange={handleChange}
+            options={{ variant: 'fullWidth' }}
+          >
+            {tabs.map((tabProps) => (
+              <TabbedLayout.Tab key={tabProps.label} {...tabProps} />
+            ))}
+          </TabbedLayout.Tabs>
+        </TabsWrapper>
+        <TabbedLayout.TabPanel value={activeTab} index={0}>
+          <p>Email</p>
+        </TabbedLayout.TabPanel>
+        <TabbedLayout.TabPanel value={activeTab} index={1}>
+          <p>Remove account</p>
+        </TabbedLayout.TabPanel>
+      </StyledCard>
     </SettingsSection>
   );
 };
